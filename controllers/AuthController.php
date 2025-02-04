a9fad18cee941522ba78285cfe915f482da63445
ABDELKEBIR HADOUI <abdelkebir123123@gmail.com>
2025-02-04 23:46:04 +0100

    Creation Controllers

---
+++ controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\controllers;
+
+use App\core\Application;
+use App\core\Request;
+use App\models\User;
+
+class AuthController 
+{
+    private User $userModel;
+    private Request $Request;
+
+    public function __construct() {
+        $this->userModel = new User();
+    }
+
+    public function Auth(){
+        return Application::$app->Router->renderView("login");
+    }
+
+    public function Login(array $data) {
+        $user = $this->userModel->authenticate($data["email"], $data["password"]);
+        if ($user) {
+            $_SESSION['user'] = $user;
+            header('Location: /dashboard');
+        } else {
+            header('Location: /login?error=invalid_credentials');
+        }
+    }
+
+    public function logout() {
+        session_destroy();
+        header('Location: /');
+    }
+}
